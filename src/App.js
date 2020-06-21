ef57631da5c53a6fc1ef50507ef3361fde3a593a
suleman97 <miansuleman1997@gmail.com>
2020-06-22 01:20:35 +0500

    content just after header is done with 6 features

--- src/App.js
+++ src/App.js
@@ -2,11 +2,19 @@ import React from 'react';
 import Header from './components/Header/header';
 import 'bootstrap/dist/css/bootstrap.css';
 
+
+import CustomCard from "./components/Content/content";
+
 function App() {
   return (
     <div className="App">
       <Header />
-      
+      <CustomCard img="img11" name={"EASILY CUSTOMIZED"} des={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."}/>
+      <CustomCard img="img12" name={"RESPONSIVE READY"} des={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."}/>
+      <CustomCard img="img13" name={"CLEAN CODE"} des={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."}/>
+      <CustomCard img="img14" name={"READY TO SHIP"} des={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."}/>
+      <CustomCard img="img15" name={"DOWNLOAD FOR FREE"} des={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."}/>
+      <CustomCard img="img16" name={"MODREN DESIGN"} des={"Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s."}/>
     </div>
   );
 }
---
+++ src/components/Content/content.js
@@ -0,0 +1,62 @@
+import React from "react";
+import './content.css';
+import img1 from './imgs/featurelogo2.gif';
+import img2 from './imgs/featurelogo3.gif';
+import img3 from './imgs/featurelogo4.gif';
+import img4 from './imgs/featurelogo5.gif';
+import img5 from './imgs/featurelogo6.gif';
+import img6 from './imgs/icon.png';
+
+// import img1 from "./imgs"
+
+class CustomCard extends React.Component {
+    constructor(props){
+        super(props)
+
+        this.state = {
+            name:"",
+            description:"",
+            img:"",
+            imgKey:{
+                "img11":img1,
+                "img12":img2,
+                "img13":img3,
+                "img14":img4,
+                "img15":img5,
+                "img16":img6,
+            }
+        }
+
+        
+    }
+
+    componentDidMount(){
+        this.setState({
+            name:this.props.name,
+            description:this.props.des,
+            img:this.props.img
+        })
+    }
+
+    render(){
+        return(
+            <React.Fragment>
+                
+                <div className="container mt-3">
+                <div className="card float-left col-md-4 mt-3" >
+                {/* <img className="card-img-top" alt="Card image" style={{width:"100%"}} /> */}
+                <div className="card-body">
+                <center>
+                    <img src={this.state.imgKey[this.state.img]}></img>
+                    <h4 className="card-title clr mt-3">{this.state.name}</h4>
+                <p className="card-text clr mt-3">{this.state.description}</p></center>
+                </div>
+                </div>
+
+                </div>
+            </React.Fragment>
+        )
+    }
+}
+
+export default CustomCard
